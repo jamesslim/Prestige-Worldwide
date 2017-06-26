3083ffda47393ad9d717be6d6e34feb7739f5fac
Logan Baker <lobaker13@gmail.com>
2017-06-26 11:53:07 -0400

    server.rb linked with erb

--- server.rb
+++ server.rb
@@ -1,3 +1,20 @@
 require 'sinatra'
 
 get '/' do
+  #If we want to specify a class for additional styling
+  #@title = "Home Page"
+  #@class_name = "home"
+  erb :home
+end
+
+get '/page1' do
+  erb :page1
+end
+
+get '/page2' do
+  erb :page2
+end
+
+get '/contact' do
+  erb :contact
+end
